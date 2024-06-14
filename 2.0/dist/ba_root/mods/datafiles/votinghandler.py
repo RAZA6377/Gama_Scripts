192778f7ff03110f69b87a5359792f541db93aa5
RAZA6377 <115006831+RAZA6377@users.noreply.github.com>
2024-06-15 00:59:37 +0530

    Added New Vote Handler

--- 2.0/dist/ba_root/mods/datafiles/votinghandler.py
+++ 2.0/dist/ba_root/mods/datafiles/votinghandler.py
@@ -2,26 +2,36 @@ import bascenev1 as bs
 import babase as ba
 import time
 
-# Will Recreate New
-vote_started = False
-voters = []
-vote_started_on = 0
-vote_cooldown = 20
+# Made by RaZa
 
-def get_player_info(clientID: int):
-    clientID = int(clientID)
-    info = {}
-    for i in bs.get_game_roster():
-        if i['client_id'] == clientID:
-            info['ds'] = i['display_string']
-            info['aid'] = i['account_id']
-            info['p'] = i['players']
-            break
-    return info
+
+vote_started = False # Vote start variable
+voters = [] # Voters list
+ongoing_vote = [] # Last vote which is running
+last_vote_end = 0 # When was last vote ended
+vote_cooldown = 20 # Voting cooldown
+current_time = int(time.time()) # current time in seconds
+
+def error(msg, cid: int):
+    broadcastmessage(msg, color=(1,0,0), transient=True, clients=[cid])
+    
+def success(msg, cid: int):
+    broadcastmessage(msg, color=(1,0,1), transient=True, clients=[cid])
+
+def get_player_info(clientID):
+        clientID = int(clientID)
+        info = {}
+        for i in _ba.get_game_roster():
+            if i['client_id'] == clientID:
+                info['ds'] = i['display_string']
+                info['aid'] = i['account_id']
+                info['p'] = i['players']
+                break
+        return info
 
 def vote_need(players):
     if players == 2:
-        return 2
+        return 1
     elif players == 3:
         return 2
     elif players == 4:
@@ -39,98 +49,74 @@ def vote_need(players):
     else:
         return players - 5
 
-def add_voter(clientid: int):
-    try:
-        playerinfo = get_player_info(clientid)
-        if clientid in voters:
-            bs.broadcastmessage("You Have Already Voted\nUse 'N' To Remove Vote", color=(1, 0, 0), transient=True, clients=[clientid])
-        else:
-            voters.append(clientid)
-            bs.broadcastmessage("Vote Accepted! Thanks For The Vote\nUse 'N' To Remove Vote", color=(1, 0, 1), transient=True, clients=[clientid])
-            total_votes = vote_need(len(bs.get_game_roster()) - 1)
-            total_voters = len(voters)
-            need_votes = total_votes - total_voters
-            if not need_votes:
-                end()
-                return None                             
-            bs.chatmessage(f"{need_votes} More Votes Needed")
-            print(f"Voter added: {playerinfo['ds']}, Total Voters: {total_voters}, Votes Needed: {need_votes}")
-    except Exception as e:
-        print(f"Error in add_voter: {e}")
-
-def remove_voter(clientid: int):
-    try:
-        playerinfo = get_player_info(clientid)
-        if clientid in voters:
-            voters.pop(clientid)
-            bs.broadcastmessage("Your Vote Is Successfully Removed\nUse 'Y' To Add Vote", color=(1, 0, 1), transient=True, clients=[clientid])
-            total_votes = vote_need(len(bs.get_game_roster()) - 1)
-            total_voters = len(voters)
-            need_votes = total_votes - total_voters
-            if not need_votes:
-                end()
-                return None
-            bs.chatmessage(f"{need_votes} More Votes Needed")
-            print(f"Voter removed: {playerinfo['ds']}, Total Voters: {total_voters}, Votes Needed: {need_votes}")
-        else:
-            bs.broadcastmessage("You Haven't Voted Yet\nUse 'Y' To Add Vote", color=(1, 0, 0), transient=True, clients=[clientid])
-    except Exception as e:
-        print(f"Error in remove_voter: {e}")
-
-def end():
-    try:
-        bs.chatmessage("End Vote Succeed")
-        with bs.get_foreground_host_activity().context:
-            bs.get_foreground_host_activity().end_game()
-        voters = []
-        vote_started = False
-    except Exception as e:
-        print(e)
-        pass
-
-def start_vote(clientid: int, msg):
-    global vote_started
-    global vote_started_on
-    global voters
-    global vote_cooldown
-    playerinfo = get_player_info(clientid)
-    if vote_started:
-        bs.broadcastmessage("A Vote Is Already Ongoing", transient=True, clients=[clientid], color=(1, 0, 0))
-        return
-    now = time.time()
-    if now < vote_started_on + vote_cooldown:
-        retry_time = int(vote_started_on + vote_cooldown - now)
-        bs.broadcastmessage(f"Vote Is On Cooldown. Retry After {retry_time}s", color=(1, 0, 0), transient=True, clients=[clientid])
-        return
-    if len(bs.get_game_roster()) - 1 == 1:
-        bs.broadcastmessage("Not Enough Players To Start A Vote", color=(1, 0, 0), transient=True, clients=[clientid])
-        return
-    else:
-        vote_started = True
-        vote_started_on = time.time()
-        bs.chatmessage("A Vote For End Is Started\nType 'Y' To Vote")
-        voters.append(clientid)
-  
-
-def handle_votes(clientid: int, msg):
-    global vote_started
-    global vote_started_on
-    global voters
-    global vote_cooldown
-    current_players = [player['client_id'] for player in bs.get_game_roster()]
-    if vote_started:
-        if vote_need(len(current_players)-1) - len(voters) <= 0:
-                end()
-                return None
-        elif msg == "Y":
-            add_voter(clientid)
-        elif msg == "N":
-            remove_voter(clientid)
-    else:
-        bs.broadcastmessage("No Vote Is Ongoing. Try Running ?end To Start", color=(1, 0, 0), transient=True, clients=[clientid])
-
-def reset():
-    
-    voters = []
+def vote_success():
+    global ongoing_vote
+    last_vote = ongoing_vote[0]
+    if last_vote == "quit":
+        bs.broadcastmessage("Restarting Server", color=(0,1,0))
+        ba.quit()
+    elif last_vote == "maxplayer":
+        limit = bs.get_foreground_host_session().max_players # before limit
+        bs.get_foreground_host_session().max_players = limit + 1 #after limit
+        bs.set_public_party_max_size = limit + 1
+        bs.broadcastmessage(f"Set Maxplayers Size To {limit}", color=(1,0,1))
+    elif last_vote == "end":
+        bs.get_foreground_host_activity().end_game()
+        bs.broadcastmessage("Ending Current Activity", color=(1,0,1))
+        
     vote_started = False
-    print("Vote Reset")
+    voters.clear()
+    last_vote_end = int(time.time())
+    ongoing_vote.clear()
+
+def check_vote(msg, client_id: int):
+    global vote_started
+    global voters
+    global vote_cooldown
+    global last_vote_end
+    global ongoing_vote
+    global current_time
+    avail_votes = ["end", "quit", "maxplayer"] # Available Votes
+    if msg in avail_votes: # if msg anyone of Available votes
+        if vote_started: # check if any vote is ongoing
+            current_vote = ongoing_vote[0] # last vote 
+            error(f"A Vote For {current_vote} Is Ongoing", client_id)
+        elif current_time <= last_vote_end + vote_cooldown: # check cooldown
+            remain_time = last_vote_end + vote_cooldown - current_time
+            error(f"Vote Is On Cooldown Try Again After {remain_time}s", client_id)
+        else:
+            vote_started = True
+            # lets save current vote mode to list
+            if msg == "end":
+                ongoing_vote.append("end")
+            elif msg == "quit":
+                ongoing_vote.append("quit")
+            elif msg == "maxplayer":
+                ongoing_vote.append("maxplayer")
+            vote_for = ongoing_vote[0]
+            bs.broadcastmessage(f"A Vote For {vote_for}\nIs Started\nType \'Y\' To Vote", color=(0,1,0))
+            
+    else:
+        if vote_started: # if vote is started 
+            if msg == "Y":
+                playerinfo = get_player_info(client_id)
+                pid = str(playerinfo['aid'])
+                if pid in voters: # if pbid in voters list send error
+                    error("You Have Already Voted", client_id)
+                    
+                else:
+                    voters.append(pid) 
+                    success("You Have Successfully Voted", client_id)
+                    need_votes = len(vote_need(len(bs.get_game_roster()) - 1)) - len(voters) # how many votes needed
+                    bs.broadcastmessage(f"{need_votes} Votes Are Needed", color=(0,1,1))
+                    if need_votes <= 0: # if need votes is 0 or greater than them xd
+                        bs.broadcastmessage("Vote Successfull", color=(0,1,1))
+                        vote_success()
+        
+# Lets Add A Function that reset votes we call this function when a new activity is started
+def reset():
+    global vote_started
+    global voters
+    vote_started = False
+    voters.clear()
+    print("Vote Reseted")
